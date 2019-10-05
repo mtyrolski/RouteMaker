f9335ca2319feecc5575440d874f563e98b88afb
mvxxx <michal.tyrolski@gmail.com>
2019-10-05 19:45:07 +0200

    vector for cstring

---
+++ src/string_vector.c
@@ -0,0 +1,79 @@
+#include "string_vector.h"
+
+StringVector* initVector(int size)
+{
+    StringVector* vector = malloc(sizeof(StringVector));
+    if(vector == NULL)
+    {
+        return NULL;
+    }
+    vector->bufferSize = size;
+    vector->size = 0;
+
+    vector->content = malloc(size*sizeof(char*));
+    if(vector->content == NULL)
+    {
+        free(vector);
+        return NULL;
+    }
+
+    for(int i = 0; i<size;i++)
+    {
+        vector->content[i] = NULL;
+    }
+    return vector;
+}
+
+StringVector* doubleVector(StringVector** vector)
+{
+    if(*vector == NULL)
+    {
+        return NULL;
+    }
+
+    StringVector* newVector = initVector((*vector)->bufferSize*2);
+
+    if(newVector == NULL)
+    {
+        return NULL;
+    }
+
+    for(int i = 0; i < (*vector)->size; i++)
+    {
+        newVector->content[i] = (*vector)->content[i];
+        newVector->size++;
+    }
+
+    free((*vector)->content);
+    free(*vector);
+    return newVector;
+}
+
+bool emplaceBack(StringVector* vector, char* element)
+{
+    vector->content[vector->size] = element;
+    vector->size++;
+    return true;
+}
+
+void resetVector(StringVector** vector, bool reinit)
+{
+    if(*vector == NULL)
+    {
+        return;
+    }
+
+    for(int i = 0; i < (*vector)->size; i++)
+    {
+        free((*vector)->content[i]);
+        (*vector)->content[i] = NULL;
+    }
+
+    free((*vector)->content);
+    free((*vector));
+    if(reinit)
+    {
+        *vector = initVector(DEFAULT_CAPACITY);
+    }
+}
+
---
+++ src/string_vector.h
@@ -0,0 +1,54 @@
+/** @file
+ * Interface of structure vector with string (char*) type
+ *
+ * @author Michał Tyrolski
+ */
+
+#ifndef __STRING_VECTOR_H__
+#define __STRING_VECTOR_H__
+
+#include "stdlib.h"
+#include "stdbool.h"
+
+/** Default capacity for vector */
+#define DEFAULT_CAPACITY 64
+
+/**
+ * Struct which represents vector of string
+ * @see std::vector from C++11 and newer
+ */
+typedef struct StringVector
+{
+    int bufferSize;     ///< size of buffer
+    int size;           ///< size of vector
+    char** content;     ///< content of vector
+}StringVector;
+
+/**
+ * @brief allocate new vector
+ * @return pointer to allocated vector
+ */
+StringVector* initVector(int size);
+
+/**
+ * @brief emplaces new element at end of the vector
+ * @param[in, out] vector - to modify
+ * @param[in] element - to emplace
+ * @return true if element was added, false otherwise
+ */
+bool emplaceBack(StringVector* vector, char* element);
+
+/**
+ * @brief resets vector, free its content
+ * @param[in, out] vector - to be cleared
+ * @param[in] reinit - informs if vector should be recreated
+ */
+void resetVector(StringVector** vector, bool reinit);
+
+/**
+ * @brief double vector and move elements from old vector to new
+ * @return pointer to new vector
+ */
+StringVector* doubleVector(StringVector** vector);
+
+#endif /*__STRING_VECTOR_H__*/
